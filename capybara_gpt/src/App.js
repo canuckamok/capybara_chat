b56422cd1644500facf93bbd9af3005654f3a497
Ilan Rotenberg <irotenberg@ip-192-168-4-21.us-west-2.compute.internal>
2023-04-23 12:41:50 -0400

    first react version

---
+++ capybara_gpt/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import './capybara.css';
+
+//import Chatbox from "./Chatbox";
+import Input from "./Input";
+
+function App() {
+  return (
+  <main>
+    <h1>CapybaraGPT</h1>
+    <Input/>
+  </main>
+  );
+}
+
+export default App;
